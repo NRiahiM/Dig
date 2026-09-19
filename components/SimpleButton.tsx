0f9a2af6e30b3d2e3a874e192cfc03c3d65d5507
NRiahiM <60337578+NRiahiM@users.noreply.github.com>
2026-09-19 11:33:25 +0330

    ساخت دکمه ساده با عنوان "ذخیره تغییرات".

---
+++ components/SimpleButton.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { Button } from "@/components/ui/button";
+
+function SimpleButton() {
+  return <Button>ذخیره تغییرات</Button>;
+}
+
+export default SimpleButton;
